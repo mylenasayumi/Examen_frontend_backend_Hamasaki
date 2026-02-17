e85bd653f607db255fe05b85fb4450cf69d9ae35
mylenasayumi <mylena.sayumi@gmail.com>
2026-02-17 18:40:34 +0100

    Suppression d'une erreur intentionnel

--- backend/backend_todo_list/backend_todo_list/urls.py
+++ backend/backend_todo_list/backend_todo_list/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from todo_list.views import health_check, trigger_error 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todo_list.urls')),
     path('health/', health_check, name='health_check'),
     path('error/', trigger_error, name='trigger_error'),
-    path('sentry-debug/', trigger_error),
 ]
