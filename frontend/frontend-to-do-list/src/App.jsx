12ecd80a609fcbd28e9702e37736c4f0079013e9
mylenasayumi <mylena.sayumi@gmail.com>
2026-02-17 21:53:26 +0100

    Test bouton bogué

--- frontend/frontend-to-do-list/src/App.jsx
+++ frontend/frontend-to-do-list/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import GestionnaireToDoList from './GestionnaireToDoList'
+import TestComponent from './TestComponent'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
         <div className="App">
             <header className="App-header"></header>
             <GestionnaireToDoList />
+            <TestComponent />
         </div>
     )
 }
