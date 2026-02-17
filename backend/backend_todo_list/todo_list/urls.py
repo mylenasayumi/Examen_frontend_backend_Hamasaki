7a8afd27b642b948493dc134e087eadf9c505de1
mylenasayumi <mylena.sayumi@gmail.com>
2026-02-17 18:54:01 +0100

    Teste Sentry

--- backend/backend_todo_list/todo_list/urls.py
+++ backend/backend_todo_list/todo_list/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('categories/<int:pk>/', views.category_detail, name='category-detail'),
     path('tasks/', views.task_list, name='task-list'),
     path('tasks/<int:pk>/', views.task_detail, name='task-detail'),
+    path('sentry-test/', views.sentry_test, name='sentry-test')
 ]
